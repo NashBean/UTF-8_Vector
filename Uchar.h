e16c6f21e103193614b0d8bc0bfa6d66f1ca636c
Nash Bean <nashbean@gmail.com>
2015-09-23 19:10:30 -0400

    Update Uchar.h

--- Uchar.h
+++ Uchar.h
@@ -17,118 +17,118 @@
 #include <vector>
 
 const int Uchar_MAJOR_VERSION = 0;
-const int Uchar_MINOR_VERSION = 6;
+const int Uchar_MINOR_VERSION = 7;
 
 const unsigned char endc= '\n';  
 
-//this is a function that turns a unacode into UTF-8 format
-extern inline void SetUnacode(std::vector<unsigned char>& uc_v, unsigned int& unacode)
+//this is a function that turns a unicode into UTF-8 format
+extern inline void Setunicode(std::vector<unsigned char>& uc_v, unsigned int& unicode)
 {   
-    if (unacode<=0x7f) 
+    if (unicode<=0x7f) 
     {
         resize(uc_v,1);    
-        uc_v[0]=unacode;
+        uc_v[0]=unicode;
     }  
-    else if (unacode>=0x80 && unacode<=0x07ff) 
+    else if (unicode>=0x80 && unicode<=0x07ff) 
     {
         resize(uc_v,2);    
-        uc_v[1]=unacode;//   &   0x3f;
+        uc_v[1]=unicode;//   &   0x3f;
         uc_v[1]=uc_v[1]  &   0x3f;
         uc_v[1]=uc_v[1]|0x80;
         
-        uc_v[0]=unacode>>6;
+        uc_v[0]=unicode>>6;
         uc_v[0]=uc_v[0]&0x1f;
         uc_v[0]=uc_v[0]|'\xc0';
     }  
-    else if (unacode>=0x0800 && unacode<=0xffff) 
+    else if (unicode>=0x0800 && unicode<=0xffff) 
     {
         resize(uc_v,3);    
         unsigned char temp;     
         
-        temp=unacode;
+        temp=unicode;
         uc_v[2]=temp  &   0x3f;
         uc_v[2]=uc_v[2]|0x80;
         
-        temp=unacode>>6;
+        temp=unicode>>6;
         uc_v[1]=temp  &   0x3f;
         uc_v[1]=uc_v[1]|0x80;
         
-        temp=unacode>>12;
+        temp=unicode>>12;
         uc_v[0]=temp&0x1f;
         uc_v[0]=uc_v[0]|0xc0;
     }  
-    else if(unacode>=0x010000 && unacode<=0x1fffff) 
+    else if(unicode>=0x010000 && unicode<=0x1fffff) 
     {
         // uc_v.reserve(4);
         resize(uc_v,4);
         
-        uc_v[3]=unacode;//   &   0x3f;
+        uc_v[3]=unicode;//   &   0x3f;
         uc_v[3]=uc_v[3]  &   0x3f;
         uc_v[3]=uc_v[3]|0x80;
         
-        uc_v[2]=unacode>>6;//   &   0x3f;
+        uc_v[2]=unicode>>6;//   &   0x3f;
         uc_v[2]=uc_v[2]  &   0x3f;
         uc_v[2]=uc_v[2]|0x80;
         
-        uc_v[1]=unacode>>12;
+        uc_v[1]=unicode>>12;
         uc_v[1]=uc_v[1]  &   0x3f;
         uc_v[1]=uc_v[1]|0x80;
         
-        uc_v[0]=unacode>>18;
+        uc_v[0]=unicode>>18;
         uc_v[0]=uc_v[0]&0x1f;
         uc_v[0]=uc_v[0]|'\xc0';
     }  
-    else if(unacode>=0x200000 && unacode<=0x03ffffff) 
+    else if(unicode>=0x200000 && unicode<=0x03ffffff) 
     {
         // uc_v.reserve(5);
         resize(uc_v,5);
         
-        uc_v[4]=unacode;//   &   0x3f;
+        uc_v[4]=unicode;//   &   0x3f;
         uc_v[4]=uc_v[4]  &   0x3f;
         uc_v[4]=uc_v[4]|0x80;
         
-        uc_v[3]=unacode>>6;//   &   0x3f;
+        uc_v[3]=unicode>>6;//   &   0x3f;
         uc_v[3]=uc_v[3]  &   0x3f;
         uc_v[3]=uc_v[3]|0x80;
         
-        uc_v[2]=unacode>>12;//   &   0x3f;
+        uc_v[2]=unicode>>12;//   &   0x3f;
         uc_v[2]=uc_v[2]  &   0x3f;
         uc_v[2]=uc_v[2]|0x80;
         
-        uc_v[1]=unacode>>18;
+        uc_v[1]=unicode>>18;
         uc_v[1]=uc_v[1]  &   0x3f;
         uc_v[1]=uc_v[1]|0x80;
         
-        uc_v[0]=unacode>>24;
+        uc_v[0]=unicode>>24;
         uc_v[0]=uc_v[0]&0x1f;
         uc_v[0]=uc_v[0]|'\xc0';
     }  
-    else if(unacode>=0x04000000 && unacode<=0x7fffffff) 
+    else if(unicode>=0x04000000 && unicode<=0x7fffffff) 
     {
         //  uc_v.reserve(6);
         resize(uc_v,6);
         
-        uc_v[5]=unacode;//   &   0x3f;
+        uc_v[5]=unicode;//   &   0x3f;
         uc_v[5]=uc_v[5]  &   0x3f;
         uc_v[5]=uc_v[5]|0x80;
         
-        uc_v[4]=unacode>>6;//   &   0x3f;
+        uc_v[4]=unicode>>6;//   &   0x3f;
         uc_v[4]=uc_v[4]  &   0x3f;
         uc_v[4]=uc_v[4]|0x80;
         
-        uc_v[3]=unacode>>12;//   &   0x3f;
+        uc_v[3]=unicode>>12;//   &   0x3f;
         uc_v[3]=uc_v[3]  &   0x3f;
         uc_v[3]=uc_v[3]|0x80;
         
-        uc_v[2]=unacode>>18;//   &   0x3f;
+        uc_v[2]=unicode>>18;//   &   0x3f;
         uc_v[2]=uc_v[2]  &   0x3f;
         uc_v[2]=uc_v[2]|0x80;
         
-        uc_v[1]=unacode>>24;
+        uc_v[1]=unicode>>24;
         uc_v[1]=uc_v[1]  &   0x3f;
         uc_v[1]=uc_v[1]|0x80;
         
-        uc_v[0]=unacode>>30;
+        uc_v[0]=unicode>>30;
         uc_v[0]=uc_v[0]&0x1f;
         uc_v[0]=uc_v[0]|'\xc0';
     }  
@@ -136,53 +136,29 @@ extern inline void SetUnacode(std::vector<unsigned char>& uc_v, unsigned int& un
     
 };
 
-extern inline const char* c_str(std::vector<unsigned char>& data)  
-{
-    if (data.size()==0) 
-    {
-        return 0x00;
-    }
-    
-    size_t sz=data.size();
-    char result[sz+1];
-    
-    for (size_t i=0;i<sz; ++i) 
-    {
-        result[i]= char(data[i]);
-    }
-    
-    result[sz]  = endc;
-    
-    return (const char*)*result;
-};
-
-extern inline void resize(std::vector<unsigned char>& uc_v,size_t x)
-{
-    if (uc_v.size()!=x) 
-    {   
-        if (uc_v.max_size()<x){  }//throw out of memory
-        else {  uc_v.resize(x); }   
-    }
-    
-};   
 
 struct  Uchar//UTF-8    container
 {
     std::vector <unsigned char>  uc_v;
-    Uchar():uc_v(0){uc_v.reserve(6)};//uc_v[0]='\x0000';};
+    Uchar():uc_v(0){uc_v.reserve(6);};//uc_v[0]='\x0000';};
     Uchar(std::vector<unsigned char>& v):uc_v(v.size())
     {
         for (size_t i=0; i<uc_v.size(); ++i) 
         {   uc_v[i]=v[i];   }
     };
-    ~Uchar(){uc_v={};};//if(uc_v.size())uc_v.clear();};
+    ~Uchar(){uc_v.clear();};//if(uc_v.size())uc_v.clear();};
     //    Uchar(Uchar& c):uc_v(c.uc_v){};
     
     Uchar(unsigned int unicode):uc_v(1)
-    {    SetUnacode(uc_v,unicode); };
+    {    Setunicode(uc_v,unicode); };
     
     void resize(size_t x)
-    {  resize(uc_v,size_t x);   };       
+    {      
+        if (uc_v.size()!=x) 
+        {   if (uc_v.max_size()<x){  }//throw out of memory
+        else {  uc_v.resize(x); }   
+    }
+   };       
     
     Uchar& operator=(const Uchar& x)
     {
@@ -198,7 +174,25 @@ struct  Uchar//UTF-8    container
         return *this;
     };
     const  char* c_str()  
-    {   return c_str(uc_v);   }
+    { 
+        if (uc_v.size()==0) 
+        {
+            return 0x00;
+        }
+        
+        size_t sz=uc_v.size();
+        char result[sz+1];
+        
+        for (size_t i=0;i<sz; ++i) 
+        {
+            result[i]= char(uc_v[i]);
+        }
+        
+        result[sz]  = endc;
+        
+        return (const char*)*result;
+
+    };
     
 };
 
@@ -206,18 +200,22 @@ struct  Ustr//UTF-8    container
 {
     std::vector <unsigned char>  str_v;
     
-    Ustr():str_v(0){str_v.reserve(6)};
+    Ustr():str_v(0){str_v.reserve(6);};
     Ustr(std::vector<unsigned char>& v):str_v(v.size())
     {
         for (size_t i=0; i<str_v.size(); ++i) 
         {   str_v[i]=v[i];   }
     };
-    ~Ustr(){str_v={};};    
-    Ustr(unsigned int unacode):str_v(1)
-    {    SetUnacode(str_v,unacode);};
+    ~Ustr(){str_v.clear();};    
+    Ustr(unsigned int unicode):str_v(1)
+    {    Setunicode(str_v,unicode);};
     
     void resize(size_t x)
-    {   resize(str_v,x)   };       
+    {  
+        if (str_v.size()!=x) 
+        {   if (str_v.max_size()<x){  }}//throw out of memory
+        else {  str_v.resize(x); }   
+   };       
     
     Ustr& operator=(const Ustr& x)
     {
@@ -234,7 +232,25 @@ struct  Ustr//UTF-8    container
     };
     
     const  char* c_str()  
-    {   return c_str(str_v);   }
+    { 
+        if (str_v.size()==0) 
+        {
+            return 0x00;
+        }
+        
+        size_t sz=str_v.size();
+        char result[sz+1];
+        
+        for (size_t i=0;i<sz; ++i) 
+        {
+            result[i]= char(str_v[i]);
+        }
+        
+        result[sz]  = endc;
+        
+        return (const char*)*result;
+        
+    };
     
 };
 
