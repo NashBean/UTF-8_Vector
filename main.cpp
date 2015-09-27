e57429c03cb5110b070eadd1682c8b739120dd79
NashBean <nashbean@gmail.com>
2015-09-26 21:19:04 -0400

    Changed test codes to include full range

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 //  UTF-8_Vector
 //
 //  Created by nash on 9/23/15.
-//  Copyright 2015 iBean_Software. All rights reserved.
+//  Copyright © 2015 iBean_Software. All rights reserved.
 //
 
 #include <iostream>
@@ -21,24 +21,24 @@ void display_bits(const Uchar& uc)
 
 int main (int argc, const char * argv[])
 {
-    Uchar  uc(0x0391);//Α
+    Uchar  uc(0x31);
     display_bits(uc);
     
-    uc=0x00a9;//©
-    display_bits(uc);
-
-    uc=0x2c02;
-    display_bits(uc);
-    
-    uc=0x13a1;
-    display_bits(uc);
-    
-    uc=0x1d230;//
-    display_bits(uc);
-
     uc=0x03a9;//Ω
     display_bits(uc);
     
+    uc=0x0800;
+    display_bits(uc);
+
+    uc=0x2b147;
+    display_bits(uc);
+    
+    uc=0x03ffffff;//max 5 byte Unicode
+    display_bits(uc);
+    
+    uc=0x7fffffff;//max Unicode
+    display_bits(uc);
+        
     return 0;
 }
 
