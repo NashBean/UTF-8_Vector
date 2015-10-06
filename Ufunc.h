06cf5664f0199f4362fad98cc3c19bc55594228d
NashBean <nashbean@gmail.com>
2015-10-05 23:14:47 -0400

    moved functs to Uchar.h.

--- Ufunc.h
+++ Ufunc.h
@@ -16,7 +16,7 @@
 
 
 const int Ufunc_MAJOR_VERSION = 0;
-const int Ufunc_MINOR_VERSION = 3;
+const int Ufunc_MINOR_VERSION = 4;
 
 namespace iBS 
 {
@@ -24,35 +24,6 @@ namespace iBS
    
     bool isOn(unsigned char byte, short index)
     { return (byte & 1<<index); };
-    //bool isLeadByte(unsigned char byte); //use ByteCount 
-    short ByteCount(unsigned char byte) 
-    {//  returns 0 if not lead byte or not UTF-8 formated 
-        if ((byte & 0x80)==0) { return 1; }
-        short result=0;
-        if (byte & 0x80) result++;
-        if (byte & 0x40) result++;
-        if (result != 2) return 0;//this was not lead byte or not UTF-8 format 
-        else return result;
-        if (byte & 0x10) result++;
-        else return result;
-        if (byte & 0x08) result++;
-        else return result;
-        if (byte & 0x04) result++;  
-        else return result;
-        if (byte & 0x02) return 0;//this was not UTF-8 format  
-        return result;
-    };
-    
-    bool isTrailByte(unsigned char byte)
-    { return (byte & 0x80)&&((byte & 0x40)==0); };
-    
-    void display_bits(iBS::Uchar& uc)
-    {
-        std::bitset<8>  bs;
-        for (size_t i=0; i<uc.size(); ++i) 
-        {   bs=uc.ref[i];   std::cout << bs << ' ';}
-        std::cout << std::endl; 
-    };
 
     void display_bits(unsigned char uc)
     {
